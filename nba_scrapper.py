2b3061a937c9525a0e45410c31c4ccb25a1dd834
R2D404 <ziwei.wei.r2d404@gmail.com>
2019-10-28 23:03:39 -0400

    init commit

---
+++ nba_scrapper.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# NBA season we will be analyzing
+year = 2019
+# URL page we will scraping (see image above)
+url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html".format(year)
+# this is the HTML from the given URL
+html = urlopen(url)
+soup = BeautifulSoup(html, features="html5lib")
+
+# use findALL() to get the column headers
+soup.findAll('tr', limit=2)
+# use getText()to extract the text we need into a list
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
+headers = headers[1:]
+print(headers)
